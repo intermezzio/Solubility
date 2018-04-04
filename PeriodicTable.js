057029a7245c80bef8cc0e92b302e61205b473fa
Andrew M <anmascillaro@ctemc.org>
2018-04-03 22:21:06 -0400

    Update Periodic Table Object
    I think we should create an object for each element, but I'm not sure if this is most efficient.

--- PeriodicTable.js
+++ PeriodicTable.js
@@ -2,9 +2,14 @@
 Create elements of the periodic table
  */
 
-var PerEl = function(name, symbol, group = 1, charge = 0) {
+var PerEl = function(name, symbol, group = 1, charge = 0, period = 1) {
 	this.name = name;
 	this.symbol = symbol;
 	this.group = group;
+	this.period = period;
 	this.charge = charge;
-}
+};
+
+PerEl.prototype.searchTerm = function(term) {
+	return (term in [this.name, this.symbol]);
+};
