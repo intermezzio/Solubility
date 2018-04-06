2c3402918036d7a1527eb4e3758ab51af4436234
Andrew M <anmascillaro@ctemc.org>
2018-04-06 00:17:29 -0400

    Fixed inputs
    Now inputs are converted into ions
    
    Still need to add solubility rules and edit string inputs

--- PeriodicTable.js
+++ PeriodicTable.js
@@ -2,14 +2,23 @@
 Create elements of the periodic table
  */
 
-var PerEl = function(name, symbol, group = 1, charge = 0, period = 1) {
+var PerEl = function(name, symbol, group = -1, charge = 0) {
 	this.name = name;
 	this.symbol = symbol;
 	this.group = group;
-	this.period = period;
 	this.charge = charge;
 };
 
 PerEl.prototype.searchTerm = function(term) {
 	return (term in [this.name, this.symbol]);
 };
+
+Ions = [];
+function addIon(name, symbol, group = -1, charge = 0) {
+	Ions.push(new PerEl(name, symbol, group, charge));
+}
+
+addIon("Hydrogen", "H", 1, 1);
+addIon("Ammonium", "NH4", 1, 1);
+
+addIon("Carbonate", "CO3", 0, -2);
